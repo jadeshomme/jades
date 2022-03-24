ac3a77a5384093be291218b1e646239dac7effd5
ngovanthuan <ngovanthuan989@gmail.com>
2022-03-24 11:05:46 +0700

    thuandaycode

--- resources/views/homeuser/layout/slide.blade.php
+++ resources/views/homeuser/layout/slide.blade.php
@@ -5,8 +5,8 @@
             <li class="bg-dark-30 bg-dark shop-page-header" style="background-image: url({{asset('/uploads/images/'.$slider->slider_img.'')}});">
                 <div class="titan-caption">
                     <div class="caption-content">
-                    <div class="font-alt mb-30 titan-title-size-1">{{$slider->name}}</div>
-                    <div class="font-alt mb-30 titan-title-size-4"> {{$slider->content}}</div>
+                    {{-- <div class="font-alt mb-30 titan-title-size-1">{{$slider->name}}</div>
+                    <div class="font-alt mb-30 titan-title-size-4"> {{$slider->content}}</div> --}}
                     {{-- <div class="font-alt mb-40 titan-title-size-1">Your online fashion destination</div><a class="section-scroll btn btn-border-w btn-round" href="#latest">Learn More</a> --}}
                     </div>
                 </div>
